41778d9943aafdf83138c8d62d7c2289b6c8c8bd
Jalen Tacsiat <jtacsiat@ada.gonzaga.edu>
2019-01-27 21:31:37 -0800

    added first half of main

--- yahtzee/src/yahtzee/Yahtzee.java
+++ yahtzee/src/yahtzee/Yahtzee.java
@@ -5,17 +5,41 @@
  */
 package yahtzee;
 
-/**
- *
- * @author jtacs
- */
-public class Yahtzee {
+import static java.lang.Math.random;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Yahtzee {
     
-}
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int diceInPlay = 5;
+        int[] hand = new int[diceInPlay];
+        random();
+        char playAgain = 'y';
+        
+        while (playAgain == 'y') {
+        String keep = "nnnnn"; //setup to roll all dice in the first roll
+        int turn = 1;
+        while (turn < 4 && keep != "yyyyy"){
+                //roll dice not kept
+            for (int dieNumber = 0; dieNumber < diceInPlay; dieNumber++)
+            {
+                if (keep.charAt(rollDie()) != 'y')
+                    hand[dieNumber] = rollDie();
+            }
+            //output roll
+            System.out.println("Your roll was: ");
+            System.out.println("Your roll was: ");
+            
+            for (int dieNumber = 0; dieNumber < diceInPlay; dieNumber++){
+                System.out.println(hand[dieNumber]);
+            }
+            //if not the last roll of the hand prompt the user for dice to keep
+            if (turn < 3){
+                System.out.println("enter dice to keep (y or n) ");
+                keep = scnr.next();
+            }
+            turn++;
+    }
+    }
+    }
